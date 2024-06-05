dcc35abd853be43b616b5c3f795a2b3084ea0539
Sonu Subedi <sonusubedi01@gmail.com>
2024-06-05 01:08:08 -0400

    handle task drags and drops

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,20 +12,27 @@ function generateTaskId() {
   localStorage.setItem('nextId', ++nextId);
 }
 
+// returns CSS classes to add based on task status
+function getTaskCardStyles(task){
+  const dueDate = dayjs(task.dueDate);
+  const today = dayjs();
+  if("done" !== task.status){
+    if(today.isAfter(dueDate, 'day')){
+      return "bg-danger text-white";
+    }else if(today.isSame(dueDate, 'day')){
+      return "bg-warning text-white";
+    }
+  }
+  return 'bg-light text-black';
+}
+
+
 // create a function to create a task card
 function createTaskCard(task) {
   // set card background color based on due date
-  let backgroundClass;
-  const dueDate = dayjs(task.dueDate);
-  const today = dayjs();
-  if(today.isAfter(dueDate, 'day')){
-    backgroundClass = "bg-danger";
-  }else if(today.isSame(dueDate, 'day')){
-    backgroundClass = "bg-warning";
-  }
   // create and append card elements
   $(`#${task.status}-cards`).append(`
-      <div id="task-${task.id}" class="card mb-3 ${backgroundClass}">
+      <div id="task-${task.id}" class="task-card card mb-3 ${getTaskCardStyles(task)}" data-id="${task.id}">
         <div class="card-header ">${task.title}</div>
           <div class="card-body">
             <p class="card-description mb-2 text-body-secondary">${task.description}</p>
@@ -34,6 +41,8 @@ function createTaskCard(task) {
         </div>
       </div>
   `);
+
+  makeTasksDraggable();
 }
 
 // TODO: create a function to render the task list and make cards draggable
@@ -100,10 +109,36 @@ function handleDeleteTask(event) {
 // TODO: create a function to handle dropping a task into a new status lane
 function handleDrop(event, ui) {
   // get the task id and new status from the event
+  const draggable = ui.draggable;
+  const taskId = draggable.attr("data-id");
+  const lane = $(this).attr('id');
+
+  draggable.css({
+    top : 0,
+    left: 0,
+    position: 'relative'
+  });
+
+  $(`#${lane}-cards`).append(draggable);
 
   // update the task status of the dragged card
+  for(let task of taskList){
+    if(task.id == taskId){
+      task.status = lane
+      $(`#task-${taskId}`).removeClass('bg-danger bg-warning bg-light text-white text-black').addClass(getTaskCardStyles(task));
+    }
+  }
 
   // save and render
+  localStorage.setItem("tasks", JSON.stringify(taskList))
+}
+
+//make task draggable
+function makeTasksDraggable() {
+ $(".task-card").draggable({
+  revert: 'invalid',
+  zIndex: 100
+ });
 }
 
 // when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
@@ -117,7 +152,12 @@ $(document).ready(function () {
 $(".swim-lanes").on('click', '#delete-task-button', handleDeleteTask);
 
   // make lanes droppable
+$(".lane").droppable({
+    accept: '.task-card',
+    drop: handleDrop
+  });
 
+  makeTasksDraggable();
 
   // make due date field a date picker
   $("#task-due-date").datepicker();
